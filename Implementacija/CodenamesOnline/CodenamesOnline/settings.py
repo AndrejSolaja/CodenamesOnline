b62e21d803960afac335ef6ed3fe7516988557b5
Teodor Djelic <dt210254d@student.etf.bg.ac.rs>
2024-05-16 12:35:39 +0200

    Registration password validator rules loosened (username and password can be similar);
    Change-Id: I9b0eccf76127f50f81130f1c3dc59cedb4079781

--- Implementacija/CodenamesOnline/CodenamesOnline/settings.py
+++ Implementacija/CodenamesOnline/CodenamesOnline/settings.py
@@ -98,9 +98,9 @@ AUTH_USER_MODEL = "home.Korisnik"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
